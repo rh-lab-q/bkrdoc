a6bc756b544c6e404392a5ec0faa545354d529ff
Jurisak <xkulda00@stud.fit.vutbr.cz>
2014-12-11 17:39:17 +0100

    modified code

--- Bachelor Thesis/parser.py
+++ Bachelor Thesis/parser.py
@@ -50,39 +50,62 @@ class parser(object):
             
     def parse_data(self):
         journal = False
-        self.outside = phase_outside(self)
+        self.phases.append(phase_outside(self))
         
         for line in self.file_test.split('\n'):
             line = line.strip()
             
-            if line[0:len("rljournalstart")].lower() == "rljournalstart":
-                journal = True
+            if line[0:1] != '#' and len(line) > 1 and \
+            not self.is_phase_journal_end(line) :
                 
-            elif line[0:len("rljournalend")].lower() == "rljournalend":
-                journal = False
-            
-            elif journal and line[0:1] != '#' and len(line) > 1 and \
-            line[0:len("rlphaseend")].lower() != "rlphaseend":
-                #print line
-                
-                if line[0:len("rlphasestartsetup")].lower() == "rlphasestartsetup":
-                    self.phases.insert(0, phase_setup(line[len("rlphasestart"):-1], self))
+                if self.is_phase_setup(line):
+                    self.phases.append(phase_setup(line[len("rlphasestart"):-1], self))
                     
-                elif line[0:len("rlphasestarttest")].lower() == "rlphasestarttest":
-                    self.phases.insert(0, phase_test(line[len("rlphasestart"):-1], self))
+                elif self.is_phase_test(line):
+                    self.phases.append(phase_test(line[len("rlphasestart"):-1], self))
                 
-                elif line[0:len("rlphasestartclean")].lower() == "rlphasestartclean":
-                    self.phases.insert(0, phase_clean(line[len("rlphasestart"):-1], self))
+                elif self.is_phase_clean(line):
+                    self.phases.append(phase_clean(line[len("rlphasestart"):-1], self))
                 
                 elif len(self.phases) > 0:
-                    self.phases[0].setup_statement(line)
+                    self.phases[-1].setup_statement(line)
             
-            elif line[0:1] != '#' and len(line) > 1 and \
-            line[0:len("rlphaseend")].lower() != "rlphaseend":
-                self.outside.setup_statement(line)
+            elif self.is_phase_journal_end(line):
+                self.phases.append(phase_outside(self))
+            
+            elif line[0:1] != '#' and len(line) > 1:
+                self.phases[-1].setup_statement(line)
+
         
     def print_statement(self):
-        self.outside.search_data()
+        #self.outside.search_data()
+        for i in self.phases:
+            print i.statement_list
+            print "\n"
+        
+    def is_phase_clean(self, line):
+        return line[0:len("rlphasestartclean")].lower() == "rlphasestartclean"
+        
+    def is_phase_test(self, line):
+        return line[0:len("rlphasestarttest")].lower() == "rlphasestarttest"
+        
+    def is_phase_setup(self, line):
+        return line[0:len("rlphasestartsetup")].lower() == "rlphasestartsetup"
+        
+    def is_phase_journal_end(self, line):
+        if line[0:len("rlphaseend")].lower() == "rlphaseend":
+            return True
+        
+        elif line[0:len("rljournalend")].lower() == "rljournalend":
+            return True
+        
+        else:
+            return False
+        
+    def is_journal_start(self, line):
+        return line[0:len("rljournalstart")].lower() == "rljournalstart"
+        
+        
         
 class phase_outside:
     """Class for searching data ourside of phases"""
@@ -100,7 +123,7 @@ class phase_outside:
         self.keywords_list = []
         
     def setup_statement(self,line):
-        self.statement_list.insert(len(self.statement_list),line)
+        self.statement_list.append(line)
         
     def search_data(self):
         for statement in self.statement_list:
@@ -112,14 +135,14 @@ class phase_outside:
             # setting variable list
             if pom == '=':
                 pom = readed.get_token()
-                self.variable_list.insert(len(self.variable_list), member)
+                self.variable_list.append(member)
                 regular = re.compile("\"(\/.*\/)(.*)\"")
                 match = regular.match(pom)
                 if match:
-                    self.variable_values_list.insert(len(self.variable_values_list), match.group(1))
-                    self.keywords_list.insert(len(self.keywords_list), match.group(2))
+                    self.variable_values_list.append(match.group(1))
+                    self.keywords_list.append(match.group(2))
                 else:
-                    self.variable_values_list.insert(len(self.variable_values_list), pom[1:-1])
+                    self.variable_values_list.append(pom[1:-1])
         
         #print self.variable_list
         #print self.variable_values_list
@@ -137,7 +160,7 @@ class phase_clean:
         self.statement_list = []
         
     def setup_statement(self,line):
-        self.statement_list.insert(len(self.statement_list),line)
+        self.statement_list.append(line)
 
 
 class phase_test:
@@ -152,7 +175,7 @@ class phase_test:
         self.statement_list = []
         
     def setup_statement(self,line):
-        self.statement_list.insert(len(self.statement_list),line)
+        self.statement_list.append(line)
 
 
 class phase_setup:
@@ -167,7 +190,7 @@ class phase_setup:
         self.statement_list = []
         
     def setup_statement(self,line):
-        self.statement_list.insert(len(self.statement_list),line)
+        self.statement_list.append(line)
         
 
 #***************** MAIN ******************
