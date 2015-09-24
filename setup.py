064d47b9a3f4bde1e389f9d9c9d146d92f89b706
Jiri Kulda <jkulda@redhat.com>
2015-09-24 14:38:29 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bkrdoc',
+    version='1.0',
+    packages=['bkrdoc', 'bkrdoc.markup'],
+    url='https://github.com/rh-lab-q/bkrdoc',
+    license='BSD',
+    author='Jiri Kulda',
+    author_email='Kulda12@seznam.cz, jkulda@redhat.com',
+    description='This project aims to provide tools for automated documentation generation for BeakerLib tests. '
+)
