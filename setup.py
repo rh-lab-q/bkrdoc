81c64ada6bcabfc4239b6cba62c7c812349a4cf7
Jiri Kulda <jkulda@redhat.com>
2015-09-25 10:11:43 +0200

    Added few information into setup.py file

--- setup.py
+++ setup.py
@@ -8,5 +8,13 @@ setup(
     license='BSD',
     author='Jiri Kulda',
     author_email='Kulda12@seznam.cz, jkulda@redhat.com',
-    description='This project aims to provide tools for automated documentation generation for BeakerLib tests. '
+    description='This project aims to provide tools for automated documentation generation for BeakerLib tests. ',
+    classifiers=["Programming Language :: Python",
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: BSD License",
+                 "Development Status :: 4 - Beta",
+                 "Topic :: Software Development :: Testing",
+                 "Topic :: Software Development :: Documentation",
+                 "Operating System :: OS Independent",
+                 "Intended Audience :: Developers"]
 )
