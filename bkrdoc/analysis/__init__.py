b7ebaaf58c898bddf13899ddf19ca971e6272b0f
Jiri Kulda <jkulda@redhat.com>
2015-09-29 16:54:53 +0200

    Rewrited imports in __init_.py files

--- bkrdoc/analysis/__init__.py
+++ bkrdoc/analysis/__init__.py
@@ -1,2 +1,16 @@
 __author__ = 'jkulda'
 
+from option_object import *
+from test_variables import *
+from test_functions import *
+from information_units import *
+from conditions_for_commands import *
+from topic_object import *
+from bkrdoc_parser import *
+from data_containers import *
+from doc_information_representation import *
+from documentation_translator import *
+from documentation_generator import *
+from get_information import *
+from Statement_data_searcher import *
+from conditions_for_commands import *
--- bkrdoc/markup/__init__.py
+++ bkrdoc/markup/__init__.py
@@ -1,1 +1,4 @@
 __author__ = 'jkulda'
+from tagged_generator import *
+from tagged_parser import *
+from data_containers import *
