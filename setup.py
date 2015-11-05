c45f3da2ad872b898c1c2b21517752605deeba8e
Jiri Kulda <jkulda@redhat.com>
2015-11-05 15:58:45 +0100

    Added new version of bkrdoc. With director file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bkrdoc',
-    version='1.2.5',
+    version='1.2.6',
     packages=['bkrdoc', 'bkrdoc.markup', 'bkrdoc.analysis'],
     url='https://github.com/rh-lab-q/bkrdoc',
     license='BSD',
@@ -17,5 +17,6 @@ setup(
                  "Topic :: Software Development :: Testing",
                  "Topic :: Software Development :: Documentation",
                  "Operating System :: OS Independent",
-                 "Intended Audience :: Developers"]
+                 "Intended Audience :: Developers"],
+    scripts=['bin/bkrdoc']
 )
